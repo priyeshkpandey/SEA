6f060c1fd86d194a39ec570f81c0cfa7195cb6bc
Priyesh Pandey <priyeshpandey@ppandey.mobileiron.dev>
2017-07-20 18:11:13 +0530

    Added model generation stubs

--- CoreEmoEngineService/src/main/java/com/services/psychological/core/CommonModels.java
+++ CoreEmoEngineService/src/main/java/com/services/psychological/core/CommonModels.java
@@ -1,16 +1,27 @@
 package com.services.psychological.core;
 
-/* *** The methods of CommonModels class should not have any parameters.
+import java.util.Random;
+
+/*
+ * *** Parameter of Integer type is mandatory that too only one 
  * *** The methods should have a return value
  * *** All method names should be unique
  * *** If you want to use the method, be sure to update the model_resources table
 
-*/public class CommonModels {
+*/
+
+public class CommonModels {
+	private Random random = new Random();
 	
-	public int getNoOfInvokedEmos(Integer currIter)
+	public int getRandomIntegerBetweenOneAndTen(Integer currIter)
 	{
-		System.out.println("Returning from CommonModels");
-		return 2;
+		System.out.println("Returning int from CommonModels");
+		return (random.nextInt(10) + 1);
+	}
+	
+	public double getValueNormalDistribution(Integer currIter) {
+		System.out.println("Returning double (normal distribution) from CommonModels");
+	    return random.nextGaussian();
 	}
 
 }
