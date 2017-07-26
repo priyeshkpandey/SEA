8dbe65ed90ab058ab8ba4eb63f15d33ba4467358
Priyesh Pandey <priyeshpandey@ppandey.mobileiron.dev>
2017-07-27 01:21:13 +0530

    added blank string check for keyVal

--- CoreEmoEngineService/src/main/java/com/services/psychological/core/SingleStepComponent.java
+++ CoreEmoEngineService/src/main/java/com/services/psychological/core/SingleStepComponent.java
@@ -302,12 +302,12 @@ public class SingleStepComponent {
 			IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException {
 		String var = keyVal.split(",")[0];
-		targetAgent = keyVal.split(",")[1] != null ? Long.parseLong(keyVal.split(",")[1]) : -1;
-		targetEvent = keyVal.split(",")[2] != null ? Long.parseLong(keyVal.split(",")[2]) : -1;
-		targetObject = keyVal.split(",")[3] != null ? Long.parseLong(keyVal.split(",")[3]) : -1;
+		targetAgent = keyVal.split(",")[1] != null &&  !keyVal.split(",")[1].equals("") ? Long.parseLong(keyVal.split(",")[1]) : -1;
+		targetEvent = keyVal.split(",")[2] != null && !keyVal.split(",")[2].equals("") ? Long.parseLong(keyVal.split(",")[2]) : -1;
+		targetObject = keyVal.split(",")[3] != null && !keyVal.split(",")[3].equals("") ? Long.parseLong(keyVal.split(",")[3]) : -1;
 		targetEmotion = keyVal.split(",")[4];
 		targetVariable = keyVal.split(",")[5];
-		sourceAgent = keyVal.split(",")[7] != null ? Long.parseLong(keyVal.split(",")[7]) : -1;
+		sourceAgent = keyVal.split(",")[7] != null && !keyVal.split(",")[7].equals("") ? Long.parseLong(keyVal.split(",")[7]) : -1;
 
 		if (!keyRowInserted.contains(keyVal)) {
 			String tableName = varsToTableMetaData.get(var).get(0);
